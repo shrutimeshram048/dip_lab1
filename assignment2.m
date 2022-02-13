98632e5b378926e8a549424c7f2a06a46d3431cc
shrutimeshram048 <98177436+shrutimeshram048@users.noreply.github.com>
2022-02-13 20:48:31 +0530

    Add files via upload

---
+++ assignment2.m
@@ -0,0 +1,42 @@
+clc
+clear all
+close all
+a = imread("girl.jpg");
+original_image=imshow(a)
+size(a)
+
+%task1
+R = a(:,:,1);
+G= a(:,:,2);
+B = a(:,:,3);
+newimage = zeros(size(a,1),size(a,2),'uint8');
+for x=1:size(a,1)
+    for y=1:size(a,2)
+        newimage(x,y) = (R(x,y).333)+(G(x,y).333)+(B(x,y)*.333);
+    end
+end
+gray_image=figure,imshow(newimage)
+size(newimage)
+
+%task2
+[r,c] = size(newimage)
+for i = 1:r
+    for j=1:c
+        f = newimage(i,j);
+        if f<127
+            newimage1(i,j)=0;
+        elseif f>127 && f<256
+            newimage1(i,j)=255;
+        end
+        
+    end
+end
+
+size(newimage1)
+
+%task3
+I1 = uint8(newimage);
+I2 = uint8(newimage1);
+image = I1 - I2;
+add_image=figure,imshow(image)
+size(image)
